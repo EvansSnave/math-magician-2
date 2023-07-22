c0a4920a8fdbbc962cd959d1e2a9da94dbad6faf
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-07-22 14:54:59 -0500

    add quotes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Calculator from './components/Calculator';
+import Quotes from './components/Quotes';
 import './components/Calculator.css';
+import './components/Quotes.css';
 
 function App() {
-  return <Calculator />;
+  return (
+    <>
+      <Calculator />
+      <Quotes />
+    </>
+  );
 }
 
 export default App;
