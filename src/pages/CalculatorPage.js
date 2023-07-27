ab3680cc23a3d3acca7ca6107797bb1f5a911e35
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-07-26 21:42:26 -0500

    implement routes

---
+++ src/pages/CalculatorPage.js
@@ -0,0 +1,10 @@
+import Calculator from '../components/Calculator';
+
+const CalculatorPage = () => (
+  <div>
+    <h1>Let´s do some math!</h1>
+    <Calculator />
+  </div>
+);
+
+export default CalculatorPage;
